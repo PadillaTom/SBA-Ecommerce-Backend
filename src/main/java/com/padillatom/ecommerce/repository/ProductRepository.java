d13a175702e7bd6eb4ed01ac9e04b7df4d03fc11
Tomas Padilla <padillatomasagustin@gmail.com>
2022-01-20 08:25:06 +0100

    Develop - Product & ProductCategory Enpoints connected to Angular

--- src/main/java/com/padillatom/ecommerce/repository/ProductRepository.java
+++ src/main/java/com/padillatom/ecommerce/repository/ProductRepository.java
@@ -2,6 +2,8 @@ package com.padillatom.ecommerce.repository;
 
 import com.padillatom.ecommerce.model.enetity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin({"http://localhost:4200"})
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
